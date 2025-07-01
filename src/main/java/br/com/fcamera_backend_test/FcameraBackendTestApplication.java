931901db86f78f645e5d456ff611092aea9071df
Pedro Antonio <antonio.pedro25@outlook.com>
2025-07-01 16:15:46 -0300

    feat: adding database configuration

---
+++ src/main/java/br/com/fcamera_backend_test/FcameraBackendTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.fcamera_backend_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcameraBackendTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcameraBackendTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fcamera_backend_test/FcameraBackendTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fcamera_backend_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcameraBackendTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
